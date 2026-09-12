eb374e79944d77c0b43e32007c419bfeee0c4ff1
Tangerine-ora <2639858834@qq.com>
2026-09-12 20:14:44 +0800

    增加修改阅读状态功能

--- manager-app/manga.js
+++ manager-app/manga.js
@@ -7,6 +7,8 @@ const recordList = document.querySelector('#record-list');
 
 let comicList = [];
 
+const statusArr = ["未读", "阅读中", "已读完"];
+
 function render() {
     recordList.innerHTML = '';
     if (comicList.length === 0) {
@@ -23,22 +25,37 @@ function render() {
         const textSpan = document.createElement('span');
         textSpan.textContent = `${comic.title} | ${comic.author} | ${comic.status}`;
 
+        const editBtn = document.createElement('button');
+        editBtn.classList.add('edit-btn');
+        editBtn.textContent = "修改状态";
+        editBtn.dataset.id = comic.id;
+
         const delBtn = document.createElement('button');
         delBtn.classList.add('del-btn');
         delBtn.textContent = "删除";
         delBtn.dataset.id = comic.id;
 
         li.appendChild(textSpan);
+        li.appendChild(editBtn);
         li.appendChild(delBtn);
         recordList.appendChild(li);
     })
 }
 
 recordList.addEventListener('click', function (e) {
+    const clickId = Number(e.target.dataset.id);
     if (e.target.classList.contains('del-btn')) {
-        const delId = Number(e.target.dataset.id);
-        comicList = comicList.filter(item => item.id !== delId);
+        comicList = comicList.filter(item => item.id !== clickId);
         render();
+    }
+    if (e.target.classList.contains('edit-btn')) {
+        const targetComic = comicList.find(c => c.id === clickId);
+        if (targetComic) {
+            let idx = statusArr.indexOf(targetComic.status);
+            idx = (idx + 1) % statusArr.length;
+            targetComic.status = statusArr[idx];
+            render();
+        }
     }
 })
 
